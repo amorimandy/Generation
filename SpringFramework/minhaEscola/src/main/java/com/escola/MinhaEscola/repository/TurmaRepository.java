dfc31266fcb712d39960102dbd5d22b5e51ff409
Andressa Amorim <dev.andymal@gmail.com>
2020-12-08 15:31:57 -0300

    add escola spring

---
+++ SpringFramework/minhaEscola/src/main/java/com/escola/MinhaEscola/repository/TurmaRepository.java
@@ -0,0 +1,15 @@
+package com.escola.MinhaEscola.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.escola.MinhaEscola.model.Turma;
+
+@Repository
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+	
+	public List<Turma> findAllByTurmaContainingIgnoreCase(String turma);;
+
+}
